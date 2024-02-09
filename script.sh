d3b90fd983cd0415930f9116aba443119890a64a
x23184396 <x23184396@student.ncirl.ie>
2024-02-09 18:54:02 +0000

    created files in folder 1 and folder 2

--- script.sh
+++ script.sh
@@ -4,3 +4,21 @@
 mkdir fol_1 fol_2
 
 echo "Folders fol_1 and fol_2 created successfully."
+# go to folder 1
+cd fol_1 || exit
+
+# Create files 1_1.txt, 1_2.txt, 1_3.txt
+touch 1_1.txt 1_2.txt 1_3.txt
+
+echo "Files 1_1.txt, 1_2.txt, 1_3.txt created in fol_1."
+
+# go to main folder
+cd ..
+
+# go to folder 2
+cd fol_2 || exit
+
+# Create files 2_1.txt, 2_2.txt, 2_3.txt
+touch 2_1.txt 2_2.txt 2_3.txt
+
+echo "Files 2_1.txt, 2_2.txt, 2_3.txt created successfully in fol_2."
